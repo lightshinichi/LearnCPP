cacd85572f556151fdcbca31a1e010502ccb208a
Naveen prakash.r <46839350+lightshinichi@users.noreply.github.com>
2023-05-12 15:51:49 +0530

    Add files via upload

---
+++ helloworld.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int man(){
+    std::cout<<"Hello World!\n";//statement in cpp
+    return 0;
+}
+
+//#include <cstdio>
+//using namespace std;
+//int main(){
+//    puts("Print Ohayo sekai");
+//    return 0;
+//}
+// for a single line comment
+
+/*multi
+  line
+  comment*/
