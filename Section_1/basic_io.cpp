b9d4b9ffbbb8f07f9e6ce81baacb8947832f73a8
Naveen Prakash.r <lightshinichi2000@gmail.com>
2023-05-13 05:34:28 +0530

    Section 1

---
+++ Section_1/basic_io.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int mai(){
+    string color;
+    cout<<"Enter you favorite color ";
+    getline(cin,color);
+    cout<< "That's great "<<color<<" is also my favorite color";
+return 0;
+}
